b8d14ade9b641adbeef84a9ad7b0bdad5f65e914
ArkV1 <Ark.V@posteo.net>
2023-04-08 00:50:43 +0800

    08/04/2023

---
+++ README.md
@@ -0,0 +1,3 @@
+# Data Bundle Builder
+
+Do not forget to install Firebase CLI or otherwise you will face authentification problems
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from google.cloud import firestore
 from google.cloud.firestore_bundle import FirestoreBundle
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="D:\Projects\Python\DataBundleBuilder/project-n1-1f74e-firebase-adminsdk-6t7o3-41f3a43480.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:\Projects\Python\DataBundleBuilder\project-n1-1f74e-firebase-adminsdk-6t7o3-b800aaa84e.json"
 
 db = firestore.Client()
 bundle = FirestoreBundle("quizzes")
@@ -13,31 +13,6 @@ docs = quizzes_ref.stream()
 for doc in docs:
     bundle_buffer: str = bundle.add_document(doc)
 
-f = open("dataBundle.txt", "w")
+f = open("data_bundle.txt", "w")
 f.write(bundle.build())
 f.close()
-
-# doc_snapshot = db.collection("quizzes").document("74WNSxi6RbYl3p378lRL").get()
-# print(doc_snapshot)
-# query = db.collection("quizzes")._query()
-# print(query)
-
-# # Build the bundle
-# # Note how `query` is named "latest-stories-query"
-# # bundle_buffer: str = bundle.add_document(doc_snapshot).add_named_query(
-# #     "latest-stories-query", query,
-# # ).build()
-
-# quizzes_ref = db.collection(u'quizzes')
-# docs = quizzes_ref.stream()
-
-# for doc in docs:
-#     bundle_buffer: str = bundle.add_document(doc).build()
-#     # print(f'{doc.id} => {doc.to_dict()}')
-
-# # for doc in bundle.documents:
-# #     print(f'{doc.id} => {doc.to_dict()}')
-
-# f = open("dataBundle.json", "w")
-# f.write("Now the file has more content!")
-# f.close()
